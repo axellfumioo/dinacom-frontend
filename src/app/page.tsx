8bdc24066fe53cfaf58f7994a915c27590ef59d4
Abshoor24 <muhammadnurulabshoor24@gmail.com>
2025-12-27 18:59:58 +0700

    UI : section3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Section1 from '@/components/landingpage/section1';
 import Section2 from '@/components/landingpage/section2';
+import Section3 from '@/components/landingpage/section3';
 
 export default function Home() {
   return (
     <div>
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   );
 }
