057e438ea99f72a98cb53d732430ecc04af36ce3
Firegame224 <xg474627@gmail.com>
2026-01-07 23:27:39 +0700

    chore: menambahkan queryProvider di root layout

---
+++ src/common/providers/QueryProvider.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+interface Props {
+    children : React.ReactNode
+}
+
+export default function QueryProvider({ children } : Props){
+    const queryClient = new QueryClient()
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
