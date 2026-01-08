1c38b59b5fb4a14556a94c3440082c487ce52eb1
Firegame224 <xg474627@gmail.com>
2026-01-08 08:57:23 +0700

    feat(auth): menambahkan custom hooks untuk register

--- src/hooks/AuthHook.ts
+++ src/hooks/AuthHook.ts
@@ -1,4 +1,4 @@
-import { LoginDto } from "@/common/dto/authDto"
+import { LoginDto, RegisterDto } from "@/common/dto/authDto"
 import { authService } from "@/services/AuthService"
 import { useMutation } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
@@ -13,6 +13,21 @@ export const useLogin = (setError: React.Dispatch<React.SetStateAction<string | 
         onSuccess: () => {
             toast.success("Login berhasil")
             router.push("/dashboard")
+        },
+        onError: (err) => {
+            setError(err.message)
+        }
+    })
+}
+
+export const useRegister = (setError: React.Dispatch<React.SetStateAction<string | null>>) => {
+    const router = useRouter()
+    return useMutation({
+        mutationKey: ['register'],
+        mutationFn: (dto: RegisterDto) => authService.register(dto),
+        onSuccess: () => {
+            toast.success("Register berhasil")
+            router.push("/auth/login")
         },
         onError: (err) => {
             setError(err.message)
