aaf8baa6ca502c1bf30fee1491bd301b4ab1c1d1
Abshoor24 <muhammadnurulabshoor24@gmail.com>
2026-01-09 13:36:24 +0700

    UI: LandingPage

--- src/components/landingpage/section2.tsx
+++ src/components/landingpage/section2.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import React from "react";
+import CountUp from "../../common/reactbits/CountUp";
 
 export default function Section2() {
   return (
-    <section className="min-h-screen snap-start flex items-center justify-center bg-yellow-section">
+    <section className="min-h-screen snap-start flex flex-col items-center justify-center bg-yellow-section">
       <h1 className="text-black font-bold text-4xl text-center">
-        130.000.000 orang di Indonesia
+         <span> 
+        <CountUp
+        from={0}
+        to={130000000}
+        separator=","
+        direction="up"
+        duration={1}
+        className="count-up-text"
+      />
+      </span> orang di Indonesia
         <br />
         memiliki masalah berat badan.
       </h1>
+      <p className="text-black font-semibold text-center mt-3">Data Olahan SKI 2023, 
+        Kementrian Kesehatan RI</p>
     </section>
   );
 }
