7e87d7b00e4fe0b7f6e32e0867978fc159e0e3fb
axel <axelazharputra@gmail.com>
2026-01-31 19:50:44 +0700

    feat: implement chat functionality and conversation management in KonsultanPage

--- src/components/landingpage/page.tsx
+++ src/components/landingpage/page.tsx
@@ -4,9 +4,9 @@ import { useRef } from "react";
 import Navbar from "@/components/landingpage/Navbar";
 
 import Section4 from "@/components/landingpage/Section4";
-import Section1 from "@/components/landingpage/Section1";
-import Section2 from "@/components/landingpage/Section2";
-import Section3 from "@/components/landingpage/Section3";
+import Section1 from "@/components/landingpage/section1";
+import Section2 from "@/components/landingpage/section2";
+import Section3 from "@/components/landingpage/section3";
 
 
 export default function LandingPage() {
